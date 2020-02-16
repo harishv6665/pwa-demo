faa6036cab32b6bb4e4c40a14b4025c558c80327
Harish <harishv6665@gmail.com>
2020-02-16 20:35:02 +0100

    sw and ui

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'shell-content';
+const filesToCache = [
+    './images/pwa.png',
+    './images/pwa-engaging.png',
+    './images/pwa-fast.png',
+    './images/pwa-reliable.png',
+    './images/icons/pwa-192.192.png',
+    './images/icons/pwa-512.512.png',
+
+    '/css/styles.css',
+
+    '/index.html',
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
